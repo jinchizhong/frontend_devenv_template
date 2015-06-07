add02fc3526a30a687fad6f85ab6cac2628bfbed
jinchizhong <jinchizhong@kingsoft.com>
2015-06-07 14:41:33 +0800

    add ejs jade static_file support

--- app.js
+++ app.js
@@ -1,9 +1,88 @@
 var express = require('express');
+var fs = require('fs');
+var jade = require('jade');
+var ejs = require('ejs');
+
 var app = express();
 
-app.get('**', function (req, res) {
-  res.send('Hello world');
+////////////////////////////////////////////////////////////////////////////////
+// options
+
+var indexes = [
+  "index.html",
+  "index.ejs",
+  "index.jade",
+];
+
+var renders = {};
+
+renders['.jade'] = function (data) {
+  var fn = jade.compile(data, {});
+  return fn({});
+};
+
+renders['.ejs'] = function (data) {
+  return ejs.compile(data)();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// implement
+
+var match_index = function (req, res, next) {
+  var path = __dirname + '/public' + req.path;
+  fs.readdir(path, function(err, files) {
+    if (err) {
+      next();
+    } else {
+      var index = "";
+      for (var i = 0; i < indexes.length; ++i) {
+        if (files.indexOf(indexes[i]) !== -1) {
+          index = indexes[i];
+          break;
+        }
+      }
+      req.url = req.url + index;
+      next();
+    }
+  });
+};
+
+// index
+app.use(function (req, res, next) {
+  var path = __dirname + '/public' + req.path;
+  fs.lstat(path, function (err, stats) {
+    if (err) {
+      next();
+    } else if (stats.isDirectory()) {
+      if (req.path.match(/\/$/)) {
+        match_index(req, res, next);
+      } else {
+        res.redirect(req.path + '/');
+      }
+    }
+  });
 });
+
+// render
+app.use(function (req, res, next) {
+  ext = req.path.match(/\.[a-zA-Z0-9]+$/)[0];
+  if (renders[ext]) {
+    var path = __dirname + '/public' + req.path;
+    fs.readFile(path, "utf-8", function (err, data) {
+      if (err) {
+        next(); 
+      } else {
+        res.send(renders[ext](data));
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public', {
+  index: false
+}));
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
